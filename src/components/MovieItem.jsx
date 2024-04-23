6be2bb2b50034fe169252fc4bc3723eb714b22b7
bakNa2t <ok2.lock@gmail.com>
2024-04-23 16:30:49 +0300

    Added MovieItem comp to MovieList comp

---
+++ src/components/MovieItem.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+function MovieItem({ movie }) {
+  MovieItem.propTypes = {
+    movie: PropTypes.object.isRequired,
+  };
+
+  return (
+    <li>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>📅</span>
+          <span>{movie.Year}</span>
+        </p>
+      </div>
+    </li>
+  );
+}
+
+export default MovieItem;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,3 +1,5 @@
+import MovieItem from "./MovieItem";
+
 import PropTypes from "prop-types";
 
 import styles from "./MovieList.module.css";
@@ -10,16 +12,7 @@ function MovieList({ movies }) {
   return (
     <ul className={styles.list}>
       {movies?.map((movie) => (
-        <li key={movie.imdbID}>
-          <img src={movie.Poster} alt={`${movie.Title} poster`} />
-          <h3>{movie.Title}</h3>
-          <div>
-            <p>
-              <span>📅</span>
-              <span>{movie.Year}</span>
-            </p>
-          </div>
-        </li>
+        <MovieItem movie={movie} key={movie.imdbID} />
       ))}
     </ul>
   );
