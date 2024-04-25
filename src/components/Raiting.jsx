7b5b8cfd9b099491011ffc1679a7af7fa1106262
bakNa2t <ok2.lock@gmail.com>
2024-04-25 10:14:12 +0300

    Update RatingIcon added extra style  prop

--- src/components/Raiting.jsx
+++ src/components/Raiting.jsx
@@ -4,12 +4,22 @@ import RatingIcon from "./RatingIcon";
 
 import PropTypes from "prop-types";
 
-function Raiting({ maxRating = 5 }) {
+function Raiting({ maxRating = 5, color = "#fcc419", size = 24 }) {
   Raiting.propTypes = {
     maxRating: PropTypes.number.isRequired,
+    color: PropTypes.string.isRequired,
+    size: PropTypes.number.isRequired,
   };
 
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
+
+  const text = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size}px`,
+  };
 
   function handleRating(rating) {
     setRating(rating);
@@ -22,11 +32,15 @@ function Raiting({ maxRating = 5 }) {
           <RatingIcon
             key={i}
             onRate={() => handleRating(i + 1)}
-            full={rating >= i + 1}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p>{rating || ""}</p>
+      <p className={text}>{tempRating || rating || ""}</p>
     </div>
   );
 }
