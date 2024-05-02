7c92fd0bf6b6605d378dc714cc18a4b9d84ec88b
bakNa2t <ok2.lock@gmail.com>
2024-05-02 14:39:10 +0300

    Update btn-toggle in BoxList adding plus/minus svg icon

--- src/components/BoxList.jsx
+++ src/components/BoxList.jsx
@@ -16,7 +16,31 @@ function BoxList({ children }) {
   return (
     <div className={styles.box}>
       <Button onClick={() => setIsOpen((open) => !open)}>
-        {isOpen ? "⮝" : "⮟"}
+        {isOpen ? (
+          <svg
+            width="16px"
+            height="16px"
+            viewBox="0 0 1024 1024"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="var(--color-background-500)"
+              d="M128 544h768a32 32 0 1 0 0-64H128a32 32 0 0 0 0 64z"
+            />
+          </svg>
+        ) : (
+          <svg
+            width="16px"
+            height="16px"
+            viewBox="0 0 1024 1024"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="var(--color-background-500)"
+              d="M480 480V128a32 32 0 0 1 64 0v352h352a32 32 0 1 1 0 64H544v352a32 32 0 1 1-64 0V544H128a32 32 0 0 1 0-64h352z"
+            />
+          </svg>
+        )}
       </Button>
       {/* <button
         className={styles["btn-toggle"]}
