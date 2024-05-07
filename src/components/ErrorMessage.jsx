983756cac1ec6af3ed888521e30ca0657bc5f00c
bakNa2t <ok2.lock@gmail.com>
2024-05-07 11:27:37 +0300

    Update ErrorMessage props

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -2,14 +2,16 @@ import styles from "./ErrorMessage.module.css";
 
 import PropTypes from "prop-types";
 
-function ErrorMessage({ message }) {
+function ErrorMessage({ message, emoji }) {
   ErrorMessage.propTypes = {
     message: PropTypes.string.isRequired,
+    emoji: PropTypes.string.isRequired,
   };
 
   return (
     <p className={styles.error}>
-      <span>🚨 </span>
+      <span role="img">{emoji} </span>
+      {/* <span role="img">🚨 </span> */}
       {message}
     </p>
   );
