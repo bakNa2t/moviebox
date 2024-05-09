e8de8cdda0fc8dbfbbbd933feb91f70788ba91a0
bakNa2t <ok2.lock@gmail.com>
2024-05-09 08:59:06 +0300

    Added classname props component to Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,13 +2,14 @@ import PropTypes from "prop-types";
 
 import styles from "./Button.module.css";
 
-function Button({ children, onClick }) {
+function Button({ children, classname, onClick }) {
   Button.propTypes = {
     children: PropTypes.node.isRequired,
+    classname: PropTypes.string.isRequired,
     onClick: PropTypes.func.isRequired,
   };
   return (
-    <button className={styles["btn-toggle"]} onClick={onClick}>
+    <button className={styles[classname]} onClick={onClick}>
       {children}
     </button>
   );
